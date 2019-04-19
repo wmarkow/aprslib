5b4421e3befd5e73c612ed579caca35b73b4d11e
Keith Packard <keithp@keithp.com>
2019-04-19 13:41:59 -0700

    Update to GPL-3 or later
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- AprsAX25.java
+++ AprsAX25.java
@@ -3,7 +3,7 @@
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful, but
@@ -15,7 +15,7 @@
 /*
  * This code uses ideas and algorithms from Dire Wolf, an amateur
  * radio packet TNC which was written by John Langner, WB2OSZ. That
- * project is also licensed under the GPL, either version 2 of the
+ * project is also licensed under the GPL, either version 3 of the
  * License or (at your option) any later version.
  */
 
---
+++ README.md
@@ -0,0 +1,10 @@
+# Aprslib — Java library for APRS decoding
+
+Aprslib is a suite of Java code designed to decode amateur radio
+position reporting system (APRS) packets from a stream of digital
+audio samples.
+
+There are lots of knobs to play with, including a number of filter
+selection options and a choice between sin/cos convolution detectors
+and Goertzel filter detectors. The former are a bit more accurate, the
+latter requires a lot less computation.
