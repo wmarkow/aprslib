8817f40264849fdb828c9a4ee1a9180ab09ffebc
Witold Markowski <wmarkowski@psi.de>
2024-11-09 19:16:20 +0000

    Add more examples. Encoding: wav file, mono, 16 bit PCM, 44100 samples per second.

--- src/test/java/aprslib_1/Launcher.java
+++ src/test/java/aprslib_1/Launcher.java
@@ -3,11 +3,16 @@ package aprslib_1;
 import org.altusmetrum.aprslib_1.AprsTest;
 
 public class Launcher {
-	
+
 	public static void main(final String[] args) {
 		AprsTest t = new AprsTest();
 
-		String[] args2 = new String[] { "src/main/resources/tnc_test01a.raw" };
+		// String[] args2 = new String[] {
+		// "src/main/resources/keith-packard/tnc_test01a.raw" };
+		// String[] args2 = new String[] { "src/main/resources/direwolf/test1.wav" };
+		// String[] args2 = new String[] {
+		// "src/main/resources/wmarkow/144800MHz_recording.wav" };
+		String[] args2 = new String[] { "src/main/resources/wmarkow/HC12_fox_example.wav" };
 
 		t.run(args2);
 	}
